68fded24f0ac4c5f2eea9932450bb098338bf77d
drewbennison <46604140+drewbennison@users.noreply.github.com>
2020-12-30 15:32:26 -0500

    Add files via upload

--- peloton.R
+++ peloton.R
@@ -1,6 +1,7 @@
-#devtools::install_github("lgellis/pelotonR")
+#devtools::install_github("lgellis/pelotonR") # might be useful but not now
 #devtools::install_github("bweiher/pelotonR") # better
 
+# packages
 library("pelotonR")
 library(tidyverse)
 library(lubridate)
@@ -8,8 +9,8 @@ library(data.table)
 library(ggpubr)
 
 #set environment variables
-Sys.setenv(PELOTON_LOGIN = "dbennison")
-Sys.setenv(PELOTON_PASSWORD = "Maddie11!")
+Sys.setenv(PELOTON_LOGIN = "")
+Sys.setenv(PELOTON_PASSWORD = "")
 
 #authorize
 peloton_auth()
@@ -17,18 +18,22 @@ peloton_auth()
 #get personal info
 me <- get_my_info() # peloton_api("api/me")
 user_id <- me$id
-num_workouts <- 100
+num_workouts <- 100 #set to max number of workouts you want to retrieve
 joins <- ""
 
 
+#how long is the workout you are doing
 length_workout_seconds <- 30*60
 
+#while your workout is happening
 for(i in seq(1, length_workout_seconds+60, 30)) {
 
+#pull in all workouts
 workouts <- peloton_api(glue::glue("/api/user/{user_id}/workouts?{joins}&limit={num_workouts}&page=0"))
 n_workouts <- length(workouts$content$data)
 
 
+#grab a couple old rides and your current ride to calculate things without errors
 all_cycle_workouts <- tibble()
 if (n_workouts > 0) {
   #for(i in c(2:n_workouts)) # for total stats
@@ -52,9 +57,10 @@ if (n_workouts > 0) {
   }
 }
 
+#get all workout ids
 workout_ids <- all_cycle_workouts$id
 
-#get performance stats
+#get performance stats for each workout
 keep <- tibble()
 for(i in c(1:length(workout_ids))) {
   message(i)
@@ -82,6 +88,7 @@ totals_summaries <- keep %>% select(id, summaries) %>% unnest(cols = c(summaries
   select(-summaries) %>% 
   pivot_wider(names_from = display_name, values_from = c(value, display_unit))
 
+#extract metrics
 metrics_summaries <- keep %>% select(id, metrics) %>% unnest(cols = c(metrics)) %>% 
   mutate(display_name = sapply(metrics,'[[',1),
          second_list = sapply(metrics,'[[',5)) %>% 
@@ -104,11 +111,12 @@ masterdata2 <- masterdata2 %>% unnest(cols = c(seconds_since_pedaling_start, Out
 masterdata2 <- masterdata2 %>% mutate(minutes = seconds_since_pedaling_start/60)
 
 
-#get current ride id
+#get current ride id of live ride
 current_ride_id <- all_cycle_workouts %>% 
   filter(is.na(end_time)) %>% 
   select(id) %>% pull()
 
+#make performance plots
 g1 <- masterdata2 %>% 
   filter(id == current_ride_id) %>% 
   ggplot(aes(x=minutes, y=Cadence)) +
@@ -130,6 +138,7 @@ g4 <-  masterdata2 %>%
   ggplot(aes(x=minutes, y=Speed)) +
   geom_line()
 
+#arrange all plots together
 figure <- ggarrange(g1, g2, g3, g4,
                     labels = c("Cadence", "Output", "Resistance", "Speed"),
                     ncol = 2, nrow = 2)
